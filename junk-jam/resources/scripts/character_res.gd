b863afe77d9bc4232917dac7a30241c33910c99c
Klarnold <141218904+Klarnold@users.noreply.github.com>
2025-11-08 10:26:02 +0300

    Revert "some"

--- junk-jam/resources/scripts/character_res.gd
+++
@@ -1,8 +0,0 @@
-class_name CharacterRes extends Resource
-
-
-@export var texture: Texture
-@export var icon_texture: Texture
-@export var name: String
-@export var last_active: String = "now"
-@export_range(-100.0, 100.0, 0.1) var affection: float
